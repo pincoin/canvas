bc59386250dc69db7f8acf7da0b4a8dec2de7b00
jonghwa <pincoins@gmail.com>
2020-10-23 01:23:13 +0900

    mockup test

--- canvas/views.py
+++ canvas/views.py
@@ -7,3 +7,8 @@ class ProductListView(generic.ListView):
 
 class ProductCustomizeView(generic.TemplateView):
     template_name = 'canvas/product_customize.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductCustomizeView, self).get_context_data(**kwargs)
+        context['IframeApi'] = 'https://h2.customerscanvas.com/Users/f7edbcda-2531-4854-b660-d84ca949dff3/SimplePolygraphy/Resources/Generated/IframeApi.js'
+        return context
